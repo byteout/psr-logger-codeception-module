e0adba4fc71c30d07de8e5639c7dc99e84bcac6a
Igor Lukic <igor@byteout.com>
2019-04-22 14:07:26 +0200

    Explicitly add phpunit as dev dependency

---
+++ tests/PsrLoggerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Byteout\Codeception\Module;
+
+use PHPUnit\Framework\TestCase;
+
+class PsrLoggerTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
